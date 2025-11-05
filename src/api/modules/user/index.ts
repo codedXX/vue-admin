821981e62c7f9eb1a1960341e89ff03c1800c500
codeYx <2584912193@qq.com>
2025-11-05 17:22:08 +0800

    提交所有

---
+++ src/api/modules/user/index.ts
@@ -0,0 +1,19 @@
+import axios from "@/api";
+
+// 登录
+export const loginAPI = (data: any) => {
+  return axios({
+    url: "/mock/login",
+    method: "post",
+    data
+  });
+};
+
+// 获取用户信息
+export const getUserInfoAPI = (params?: any) => {
+  return axios({
+    url: "/mock/user/getUserInfo",
+    method: "get",
+    params
+  });
+};
---
+++ src/layout/components/Footer/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <a-layout-footer class="footer">
+    <div class="footer_title" @click="onFooter">2024 © SnowAdmin by 兔子先森</div>
+  </a-layout-footer>
+</template>
+
+<script setup lang="ts">
+const onFooter = () => {
+  window.open("https://github.com/WangFan-io/SnowAdmin", "_blank");
+};
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  height: 30px;
+  border-top: $border-1 solid $color-border-2;
+}
+.footer_title {
+  margin: 0 auto;
+  color: $color-text-4;
+  text-align: center;
+  cursor: pointer;
+}
+</style>
